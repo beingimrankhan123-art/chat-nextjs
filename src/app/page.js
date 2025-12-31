0619de08131fe642ee08e883ea4c487ad1123abf
Md Imran <beingimrankhan123@gmail.com>
2025-12-31 22:54:42 +0530

    added login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { redirect } from "next/navigation";
+import LoginForm from "./components/auth/LoginForm";
 
 export default function Home() {
-  return (
-    <div>
-      <div className={styles.page}>
-        Chup
-      </div>
-      <div>
-        Kam kar
-      </div>
-    </div>
-  );
+  redirect("/login");
 }
